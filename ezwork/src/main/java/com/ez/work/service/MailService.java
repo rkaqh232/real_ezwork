5afe3571d8bac2764b17ee596b98618369814d55
sujin <sujin@DESKTOP-NSUB41V>
2020-07-06 17:48:36 +0900

    메일

--- ezwork/src/main/java/com/ez/work/service/MailService.java
+++ ezwork/src/main/java/com/ez/work/service/MailService.java
@@ -1,7 +1,11 @@
 package com.ez.work.service;
 
+import java.util.List;
+
 import com.ez.work.domain.Mail;
 
 public interface MailService {
 	public void insertMail(Mail mail);
+	public List<Mail> getMailList(int page, int limit);
+	public int getListCount();
 }

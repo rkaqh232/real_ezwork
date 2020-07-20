0b1f78157279f412e8867fddde7e67aca76ecd42
user1 <user1@DESKTOP-B0UTSFI>
2020-07-20 15:30:35 +0900

    appr sql 수정.

--- ezwork/src/main/webapp/resources/sql/approve.sql
+++ ezwork/src/main/webapp/resources/sql/approve.sql
@@ -3,11 +3,15 @@ drop table approve;
 create table approve(
 	APPR_CODE		NUMBER			PRIMARY KEY,	--결재 번호
 	M_CODE			VARCHAR2(50),					--사원 코드(id)
+	M_PART			VARCHAR2(30),
 	APPR_TITLE		VARCHAR2(200),					--제목
 	APPR_CONTENT	VARCHAR2(1000),					--내용
 	FIRST_CODE		VARCHAR2(50),					--1-사원
+	M_PART_F		VARCHAR2(30),
 	SECOND_CODE		VARCHAR2(50),					--2-사원
+	M_PART_S		VARCHAR2(30),
 	THIRD_CODE		VARCHAR2(50),					--3-사원
+	M_PART_T		VARCHAR2(30),
 	APPR_FILE		VARCHAR2(500),					--변경된 파일명
 	APPR_ORIGIN		VARCHAR2(500),					--original 파일명
 	APPR_STAT		NUMBER,							--결재 상태

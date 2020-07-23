e9a07e2065e0474abb8553d5e4306ad4d3ddb096
anne809 <unnuagej@gmail.com>
2020-07-23 17:04:45 +0900

    7/23 사원 명부 / 조회페이지

--- ezwork/src/main/java/com/ez/work/controller/MemberController.java
+++ ezwork/src/main/java/com/ez/work/controller/MemberController.java
@@ -244,52 +244,99 @@ public class MemberController {
 		}
 		
 		
-		// 사원등록페이지
+		// 사원 조회 리스트
 		@GetMapping(value = "/list.hr")
 		public String member_list(HttpServletRequest request, Model m) {
-			m.addAttribute("page", "member/member_list2.jsp");
+			m.addAttribute("page", "member/member_list3.jsp");
 			return "home";
 		}
 		
+		
+		
+		@RequestMapping(value = "/member_list.net")
+		public ModelAndView memberList(
+
+				@RequestParam(value = "page", defaultValue = "1", required = false) int page,
+				@RequestParam(value = "limit", defaultValue = "3", required = false) int limit, ModelAndView mv,
+				@RequestParam(value = "search_field", defaultValue = "-1") int index,
+				@RequestParam(value = "search_word", defaultValue = "") String search_word) throws Exception {
+
+			List<Member> list = null;
+			int listcount = 0;
+
+			list = loginmemberservice.getSearchList2(index, search_word, page, limit);
+			listcount = loginmemberservice.getSearchListCount(index, search_word); // 총 리스트 수를 받아옴
+
+			// 총 페이지 수
+			int maxpage = (listcount + limit - 1) / limit;
+
+			// 현재 페이지에 보여줄 시작 페이지 수
+			int startpage = ((page - 1) / 10) * 10 + 1;
+
+			// 현재 페이지에 보여줄 마지막 페이지 수(10, 20, 30 등...)
+			int endpage = startpage + 10 - 1;
+
+			if (endpage > maxpage)
+				endpage = maxpage;
+
+			mv.setViewName("member/member_list");
+			mv.addObject("page", page);
+			mv.addObject("maxpage", maxpage);
+			mv.addObject("startpage", startpage);
+			mv.addObject("endpage", endpage);
+			mv.addObject("listcount", listcount);
+			mv.addObject("memberlist", list);
+			mv.addObject("limit", limit);
+			mv.addObject("search_field", index);
+			mv.addObject("search_word", search_word);
+			return mv;
+		}
+		
+		
 	/*
-	 * //검색 리스트
-	 * 
-	 * @GetMapping(value = "/list.hr")
-	 * //@RequestMapping(value="/member_list.net.net") public String member_list(
-	 * 
-	 * @RequestParam(value="page",defaultValue="1",required=false) int page,
-	 * 
-	 * @RequestParam(value="limit", defaultValue = "3", required = false) int limit,
-	 * Model m,
-	 * 
-	 * @RequestParam(value="search_field", defaultValue="-1") int index,
-	 * 
-	 * @RequestParam(value="search_word", defaultValue="") String search_word )
-	 * throws Exception {
-	 * 
-	 * List<Member> list = null; int listcount = 0;
-	 * 
-	 * list = loginmemberservice.getSearchList(index, search_word, page, limit);
-	 * listcount = loginmemberservice.getSearchListCount(index, search_word); // 총
-	 * 리스트 수를 받아옴
-	 * 
-	 * // 총 페이지 수 int maxpage = (listcount + limit - 1) / limit;
-	 * 
-	 * // 현재 페이지에 보여줄 시작 페이지 수 int startpage = ((page - 1) / 10) * 10 + 1;
-	 * 
-	 * // 현재 페이지에 보여줄 마지막 페이지 수(10, 20, 30 등...) int endpage = startpage + 10 - 1;
-	 * 
-	 * if(endpage > maxpage) endpage = maxpage;
-	 * 
-	 * m.addAttribute("member/member_list"); m.addAttribute("page", page);
-	 * m.addAttribute("maxpage", maxpage); m.addAttribute("startpage", startpage);
-	 * m.addAttribute("endpage", endpage); m.addAttribute("listcount", listcount);
-	 * m.addAttribute("memberlist", list); m.addAttribute("limit", limit);
-	 * m.addAttribute("search_field", index); m.addAttribute("search_word",
-	 * search_word);
-	 * 
-	 * 
-	 * return mv; }
+		 //검색 리스트
+		  @GetMapping(value = "/member_list.net")
+		  public String member_list(
+		         @RequestParam(value="page",defaultValue="1",required=false) int page,
+		         @RequestParam(value="limit", defaultValue = "3", required = false) int limit,
+		         Model m,
+		         @RequestParam(value="search_field", defaultValue="-1") int index,
+		         @RequestParam(value="search_word", defaultValue="") String search_word
+		         ) throws Exception {
+		      
+		      List<Member> list = null;
+		      int listcount = 0;
+		      
+		      list = loginmemberservice.getSearchList(index, search_word, page, limit);
+		      listcount = loginmemberservice.getSearchListCount(index, search_word); // 총 리스트 수를 받아옴
+		      
+		      // 총 페이지 수
+		      int maxpage = (listcount + limit - 1) / limit;
+		      
+		      // 현재 페이지에 보여줄 시작 페이지 수
+		      int startpage = ((page - 1) / 10) * 10 + 1;
+		      
+		      // 현재 페이지에 보여줄 마지막 페이지 수(10, 20, 30 등...)
+		      int endpage = startpage + 10 - 1;
+		      
+		      if(endpage > maxpage)
+		         endpage = maxpage;
+		      
+		      m.addAttribute("page","member/member_list");
+		      m.addAttribute("page", page);
+		      m.addAttribute("maxpage", maxpage);
+		      m.addAttribute("startpage", startpage);
+		      m.addAttribute("endpage", endpage);
+		      m.addAttribute("listcount", listcount);
+		      m.addAttribute("memberlist", list);
+		      m.addAttribute("limit", limit);
+		      m.addAttribute("search_field", index);
+		      m.addAttribute("search_word", search_word);
+		      return "home";
+		      
+		      
+				
+		   }
 	 */
 		  
 		  
